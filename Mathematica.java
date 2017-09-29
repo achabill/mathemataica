4d903963095b7a3124d99990ee7598d632edbeb9
Acha Jackson <achajackson@gmail.com>
2017-09-29 03:09:39 +0100

    created class Mathematica, added method power() and other mathematical functions

--- Mathematica.java
+++ Mathematica.java
@@ -2,4 +2,33 @@ public class Mathematica{
     public static int sum(int a, int b){
         return a + b;
     }
+    public static int power (int a, int b) {
+        int result = a;
+        for (int i = 1; i <= b; i++){
+            result *= a;
+        }
+        return result;
+    }
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+    public static int multiplication(int a, int b){
+        return a * b;
+    }
+    public static int division(int a, int b){
+        return a / b;
+    }
+    public static int Abs(int x){
+        if (x > 0)
+            return x;
+        else
+            return x * -1;
+    }
+    public static void factors(int x){
+        for (int i = 1; i <= x; i++){
+            if (x % i == 0){
+                System.out.printf("factors of %d are : %d , ",x,i);
+            }
+        }
+    }
 }
--- Test.java
+++ Test.java
@@ -2,5 +2,18 @@ public class Test{
     public static void main(String[] args){
         int r = Mathematica.sum(1,2);
         System.out.println("sum(1,2) : " + r);
+
+        r = Mathematica.Abs(-1);
+        System.out.println("Abs(-1) : "+ k);
+
+        r = Mathematica.division(4, 2);
+        System.out.println("division(4 , 2) : " + r);
+
+        r = Mathematica.power(3, 5);
+        System.out.println("power(3 , 5) : " + r);
+
+        Mathematica.factors(12);
+
+
     }
 }
